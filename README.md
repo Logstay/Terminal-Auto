12623f6cee46a9e400cd32302e39ba11b5d78f83
Alan <lanzn16@outlook.com>
2020-01-28 23:46:14 -0300

    add function init four projects auto

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Terminal-Auto
-# Terminal-Auto
+
+
+# tool to aid project startup via shell script
+
+install xdotool and terminator to use the tool:
+
+# xdotool
+sudo apt-get install xdotool
+
+# terminator
+Add Terminator Repository:
+sudo add-apt-repository ppa:gnome-terminator
+Update sources.list
+sudo apt-get update
+Install Terminator
+sudo apt-get install terminator
+
+
+for initialize, in terminal read $ sh /home/<username>/Terminal-Auto/init-check.sh
--- init-check.sh
+++ init-check.sh
@@ -1,4 +1,41 @@
 #!/bin/sh
+# func var (dotenv)
+read_var() {
+    VAR=$(grep $1 $2 | xargs)
+    IFS="=" read -ra VAR <<< "$VAR"
+    echo ${VAR[1]}
+}
+# vars
+MY_FOLDER=
+MY_FOLDER_PROJECT_1=
+MY_FOLDER_PROJECT_2=
+MY_FOLDER_PROJECT_3=
+MY_FOLDER_PROJECT_4=
+MY_USER=
+MY_COMMAND=
+# init terminator
 terminator
-xdotto
-cd www/hello
+# init PROJECT service
+xdotool sleep 0.5 type "cd /home/$MY_USER/$MY_FOLDER/$MY_FOLDER_PROJECT_1"
+xdotool sleep 0.5 key KP_Enter;
+xdotool sleep 0.5 type "$MY_COMMAND"
+xdotool sleep 0.5 key KP_Enter;
+xdotool sleep 0.5 key ``ctrl+shift+e''; 
+# init PROJECT check-service
+xdotool sleep 0.5 type "cd /home/$MY_USER/$MY_FOLDER/$MY_FOLDER_PROJECT_2"
+xdotool sleep 0.5 key KP_Enter;
+xdotool sleep 0.5 type "$MY_COMMAND"
+xdotool sleep 0.5 key KP_Enter;
+xdotool sleep 0.5 key ``ctrl+shift+o'';
+# FOLDER PROJECT mobile-check-api
+xdotool sleep 0.5 type "cd /home/$MY_USER/$MY_FOLDER/$MY_FOLDER_PROJECT_3"
+xdotool sleep 0.5 key KP_Enter;
+xdotool sleep 0.5 type "$MY_COMMAND"
+xdotool sleep 0.5 key KP_Enter;
+xdotool sleep 0.5 key ``alt+tab'';
+xdotool sleep 0.5 key ``ctrl+shift+o'';
+# FOLDER PROJECT config-server
+xdotool sleep 0.5 type "cd /home/$MY_USER/$MY_FOLDER/$MY_FOLDER_PROJECT_4"
+xdotool sleep 0.5 key KP_Enter;
+xdotool sleep 0.5 type "$MY_COMMAND"
+xdotool sleep 0.5 key KP_Enter;
